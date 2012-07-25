a8551601a92c341e20d5b4b0065f5ca8d5547a82
ckutum <ckutum@163.com>
2012-07-25 15:53:44 +0800

    222

---
+++ BWMTest/COTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  COTestViewController.h
+//  BWMTest
+//
+//  Created by 陈渊佑 on 12-7-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface COTestViewController : UIViewController
+
+@end
---
+++ BWMTest/COTestViewController.m
@@ -0,0 +1,43 @@
+//
+//  COTestViewController.m
+//  BWMTest
+//
+//  Created by 陈渊佑 on 12-7-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "COTestViewController.h"
+
+@interface COTestViewController ()
+
+@end
+
+@implementation COTestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
